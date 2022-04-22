7431aceb093d2ed45221911c8ccc565bc65f3f84
Ronnie Myers <hi@ronniemyers.com>
2022-04-22 11:57:13 -0500

    add duckduckgo

---
+++ scripts/duckduckgoTheme.js
@@ -0,0 +1,5 @@
+/* visit https://duckduckgo.com/?k7=282a36&k8=f8f8f2&k9=50fa7b&kae=t&kt=p&ks=m&kw=n&km=l&ko=s&kj=282a36&ka=p&kaa=bd93f9&ku=-1&kx=f1fa8c&ky=44475a&kaf=1&kai=1&kf=1
+then paste the following script in your browser console
+*/
+
+var dracula=["7=282a36","8=f8f8f2","9=50fa7b","ae=t","t=p","s=m","w=n","m=l","o=s","j=282a36","a=p","aa=bd93f9","u=-1","x=f1fa8c","y=44475a","af=1","ai=1","f=1"];for(var i=0;i<dracula.length;i++)document.cookie=dracula[i];alert('Appearance settings have successfully been updated!');location.reload();
---
+++ scripts/update.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+sudo softwareupdate -ia --verbose ; brew bundle -v ; brew cleanup ; brew doctor --verbose
